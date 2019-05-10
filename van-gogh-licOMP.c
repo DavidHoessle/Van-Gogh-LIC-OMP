3937e7106fad0f1f7fb211e4abe88038a4e5816b
David Hössle <dahoit04@hs-esslingen.de>
2019-05-10 15:48:53 +0200

    added parallelisation for rgb_to_hsl function (l. 434)

--- van-gogh-licOMP.c
+++ van-gogh-licOMP.c
@@ -430,6 +430,7 @@ lic_image (GimpPixelRgn *src_rgn,
   *color = col;
 }
 
+// OMP
 static guchar*
 rgb_to_hsl (GimpDrawable     *drawable,
             LICEffectChannel  effect_channel)
@@ -452,35 +453,36 @@ rgb_to_hsl (GimpDrawable     *drawable,
 
   themap = g_new (guchar, maxc);
 
-  for (y = 0; y < region.h; y++)
-    {
-      for (x = 0; x < region.w; x++)
-        {
-          data[3] = 255;
+  #pragma omp parallel for private(y, x, data, val, index) collapse(2)
+    for (y = 0; y < region.h; y++)
+      {
+        for (x = 0; x < region.w; x++)
+          {
+            data[3] = 255;
 
-          gimp_pixel_rgn_get_pixel (&region, data, x, y);
-          gimp_rgba_set_uchar (&color, data[0], data[1], data[2], data[3]);
-          gimp_rgb_to_hsl (&color, &color_hsl);
+            gimp_pixel_rgn_get_pixel (&region, data, x, y);
+            gimp_rgba_set_uchar (&color, data[0], data[1], data[2], data[3]);
+            gimp_rgb_to_hsl (&color, &color_hsl);
 
-          switch (effect_channel)
-            {
-            case LIC_HUE:
-              val = color_hsl.h * 255;
-              break;
-            case LIC_SATURATION:
-              val = color_hsl.s * 255;
-              break;
-            case LIC_BRIGHTNESS:
-              val = color_hsl.l * 255;
-              break;
-            }
+            switch (effect_channel)
+              {
+              case LIC_HUE:
+                val = color_hsl.h * 255;
+                break;
+              case LIC_SATURATION:
+                val = color_hsl.s * 255;
+                break;
+              case LIC_BRIGHTNESS:
+                val = color_hsl.l * 255;
+                break;
+              }
 
-          /* add some random to avoid unstructured areas. */
-          val += g_rand_double_range (gr, -1.0, 1.0);
+            /* add some random to avoid unstructured areas. */
+            val += g_rand_double_range (gr, -1.0, 1.0);
 
-          themap[index++] = (guchar) CLAMP0255 (RINT (val));
-        }
-    }
+            themap[index++] = (guchar) CLAMP0255 (RINT (val));
+          }
+      }
 
   g_rand_free (gr);
 
